b12abf03eef3defd19583fd39285ffa795129159
Gage Benne <gagedbenne@gmail.com>
2017-10-31 21:35:11 -0500

    Routing for nested bathroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root 'buildings#index'
 
-  resources :buildings
+  resources :buildings do
+    resources :bathrooms
+  end
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'buildings#index'
 
-  resources :buildings
 end
