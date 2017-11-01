5b180920c75412b3c65a04caa234278b06927c4b
Zach Bruennig <zach@Zachs-MacBook-Air.local>
2017-11-01 12:06:47 -0500

    Added validation methods for buildings, checking for empty names and duplicate buildings

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,37 @@
 class Building < ApplicationRecord
   has_many :bathrooms
+
+  :strip_input
+
+  before_save :strip_input
+  #Validates data before entering it into the db,
+  #checks that each field exists and name is unique
+  #validates :name, uniqueness: {message: "Building already exists!"}, on: :create
+  validate :name_cannot_be_bad, :address_must_exist
+
+  private
+
+  #removes spaces, tabs, etc from beginning and end of input
+  def strip_input
+    self.name = self.name.strip
+    self.address = self.address.strip
+  end
+
+  #makes sure building name is not empty, and name is unique
+  def name_cannot_be_bad
+    if self.name.strip == ""
+      self.errors[:base] << "Building name cannot be empty!"
+    elsif Building.exists? name: self.name.strip
+      self.errors[:base] << self.name + " already exists!"
+    end
+  end
+
+  #make sure address is not empty
+  def address_must_exist
+    if self.address.strip == ""
+      self.errors[:base] << "Building must have an address!"
+    end
+  end
+
+
 end
