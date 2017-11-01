2fb2536f71ec5c0e237f71528ca270b3cbabad60
Gage Benne <gagedbenne@gmail.com>
2017-10-31 20:03:27 -0500

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101000226) do
+ActiveRecord::Schema.define(version: 20171101003226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bathrooms", force: :cascade do |t|
+    t.string   "floor"
+    t.string   "location"
+    t.string   "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "buildings", force: :cascade do |t|
     t.string   "name"
