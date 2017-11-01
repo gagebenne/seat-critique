a6b11f8f0b8532da5fa74bf16c921302c5ecb7ce
Andy <amonroe805@ku.edu>
2017-10-31 20:09:54 -0500

    Add a few basic gems

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,29 @@
 class BuildingsController < ApplicationController
+
+  def index
+    @buildings = Building.all
+  end
+
+  def show
+    @building = Building.find(params['id'])
+  end
+
+  def new
+    @building = Building.new
+  end
+
+  def create
+    @building = Building.new(building_params)
+    if @building.save
+      redirect_to(buildings_path)
+    else
+      redirect_to(buildings_new_path)
+    end
+  end
+
+  private
+
+  def building_params
+    params.require(:building).permit(:name,:address)
+  end
 end
