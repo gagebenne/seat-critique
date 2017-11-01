e4f74a98b554da52a84188d67a23a591213798b7
Zach Bruennig <zach@Zachs-MacBook-Air.local>
2017-11-01 14:17:16 -0500

    Editing building info now validates as intended, views are updated to show error messages.

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,13 +1,12 @@
 class Building < ApplicationRecord
   has_many :bathrooms
 
-  :strip_input
-
   before_save :strip_input
+
   #Validates data before entering it into the db,
-  #checks that each field exists and name is unique
-  #validates :name, uniqueness: {message: "Building already exists!"}, on: :create
-  validate :name_cannot_be_bad, :address_must_exist
+  #checking that each field exists and name is unique
+  validate :name_cannot_be_bad, :address_must_exist, on: :create
+  validate :edit_valid_name, :address_must_exist2, on: :update
 
   private
 
@@ -20,18 +19,36 @@ class Building < ApplicationRecord
   #makes sure building name is not empty, and name is unique
   def name_cannot_be_bad
     if self.name.strip == ""
-      self.errors[:base] << "Building name cannot be empty!"
+      self.errors[:base] << "Building name cannot be empty"
     elsif Building.exists? name: self.name.strip
-      self.errors[:base] << self.name + " already exists!"
+      self.errors[:base] << self.name.strip + " already exists"
     end
   end
 
   #make sure address is not empty
   def address_must_exist
     if self.address.strip == ""
-      self.errors[:base] << "Building must have an address!"
+      self.errors[:base] << "Building must have an address"
     end
   end
 
+  #For some reason, validation doesn't work when two validators call
+  #the same method. So, this simply redirects to the other one.
+  def address_must_exist2
+    address_must_exist
+  end
+
+  #when editing a building, the name can be the old name, but no other
+  #names in the database. We check that here.
+  def edit_valid_name
+    if self.name.strip == ""
+      self.errors[:base] << "Building name cannot be empty"
+    elsif Building.exists? name: self.name.strip
+      oldName = Building.find(self.id).name
+      if self.name.strip != oldName
+        self.errors[:base] << self.name.strip + " already exists"
+      end
+    end
+  end
 
 end
