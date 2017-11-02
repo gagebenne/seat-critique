f7b7c8a501becadb4c3f132cc6ea385e7dda00bf
zbruennig <zkb@ku.edu>
2017-11-01 21:40:19 -0500

    Bathrooms now validated for all fields to be non-empty, formatted properly, and also standardizes input

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -2,16 +2,16 @@ class Bathroom < ApplicationRecord
   belongs_to :building
   has_many :critiques
 
-  validate :gender_options
+  validate :gender_options, :valid_floor, :validate_location
 
-  before_save :strip_input, :unify_gender
+  before_save :format_input, :unify_gender
 
   private
 
   #removes spaces, tabs, etc from beginning and end of input
-  def strip_input
+  def format_input
     self.gender = self.gender.strip
-    self.floor = self.floor.strip
+    self.floor = self.floor.strip.upcase
     self.location = self.location.strip
   end
 
@@ -49,7 +49,27 @@ class Bathroom < ApplicationRecord
       end
     end
     if(!inList)
-      self.errors[:base] << "Gender not recognized. Please use Male or Female"
+      self.errors[:base] << "Gender not recognized; please use Male or Female"
     end
   end
+
+  def valid_floor
+    name = self.floor.strip
+    if name == ""
+      self.errors[:base] << "Must provide a floor number"
+    elsif !is_number?(name) && name.casecmp("L") != 0 && name.casecmp("LL") != 0 && name.casecmp("B") != 0
+      self.errors[:base] << "Invalid floor"
+    end
+  end
+
+  def is_number?(obj)
+    return obj.to_s == obj.to_i.to_s
+  end
+
+  def validate_location
+    if self.location.strip == ""
+      self.errors[:base] << "Must specify a location"
+    end
+  end
+
 end
