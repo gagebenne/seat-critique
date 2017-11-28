8e71c458ff3d05ca6933248e890f8f0f82feb412
Andy <amonroe805@ku.edu>
2017-11-28 06:49:38 -0600

    Remove gender methods. User now must select gender from three options.

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -2,11 +2,11 @@ class Bathroom < ApplicationRecord
   belongs_to :building
   has_many :critiques
 
-  validate :gender_options, :valid_floor
+  validate :valid_floor
 
   validates :gender, :floor, :location, presence: true
 
-  before_save :format_input, :unify_gender
+  before_save :format_input
 
   private
 
@@ -15,33 +15,6 @@ class Bathroom < ApplicationRecord
     self.gender = self.gender.strip
     self.floor = self.floor.strip.upcase
     self.location = self.location.strip
-  end
-
-  #Converts all allowable gender inputs into either
-  #'Male' or 'Female'
-  def unify_gender
-    input = self.gender.downcase
-    male = ["man","men","male","males","m","boy","boys","guy","guys"]
-    female = ["woman","women","female","females","f","girl","girls","lady","ladies"]
-    unisex = ["unisex","nonbinary","non-binary"]
-    if male.include?(input)
-      self.gender = "Male"
-    elsif female.inlcude?(input)
-      self.gender = "Female"
-    elsif unisex.include?(input)
-      self.gender = "Unisex"
-    end
-  end
-
-  #Forces gender to be from a prespecified set of allowed inputs
-  #Validation makes sure it's on this list
-  def gender_options
-    recognized_genders = ["men","male","males","m","boy","boys","guy","guys","man",
-      "woman","women","female","females","f","girl","girls","lady","ladies",
-      "unisex","nonbinary","non-binary"]
-    unless recognized_genders.include?(self.gender.downcase)
-      self.errors[:base] << "Gender not recognized; please use Male, Female, or Unisex"
-    end
   end
 
   def valid_floor
