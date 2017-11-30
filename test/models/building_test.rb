77d8d7f21f95336c11e5a1273de5c15ed63e29d4
zbruennig <zkb@ku.edu>
2017-11-30 03:48:49 -0600

    Added extra test for validation messages, can't figure out why the truthy tests are failing

--- test/models/building_test.rb
+++ test/models/building_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class BuildingTest < ActiveSupport::TestCase
 
+  test 'should have the necessary required validators' do
+    building = Building.new
+    assert_not building.valid?
+    assert_equal [:name, :address], building.errors.keys
+  end
+
   test 'cannot save empty building' do
     building = Building.new
     assert_not building.save
@@ -28,5 +34,5 @@ class BuildingTest < ActiveSupport::TestCase
     building.save
     assert_not building2.save
   end
-  
+
 end
